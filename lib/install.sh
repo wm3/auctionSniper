f39a2598135736db5b2866593e7fe14f1964ac20
yujiorama <yujiorama+github@gmail.com>
2012-02-21 13:18:36 +0900

    いろいろと不足してて残念だったので追加

--- lib/install.sh
+++ lib/install.sh
@@ -2,3 +2,4 @@
 mvn install:install-file -Dfile=lib/windowlicker-swing-DEV.jar -DgroupId=com.objogate.wl -DartifactId=windowlickr-swing -Dversion=1.0.0 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/windowlicker-core-DEV.jar -DgroupId=com.objogate.wl -DartifactId=windowlickr-core -Dversion=1.0.0 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/windowlicker-web-DEV.jar -DgroupId=com.objogate.wl -DartifactId=windowlickr-web -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/tools.jar -DgroupId=com.objogate.wl -DartifactId=windowlickr-tools -Dversion=1.0.0 -Dpackaging=jar
